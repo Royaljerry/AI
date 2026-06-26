1a1af95f043dbaffd3509ff62ddaff091b4e7041
Adam Pócs <adam.pocs@edreamsodigeo.com>
2026-06-26 19:58:40 +0200

    refactor(illustrator): optimize layer manipulation script

---
+++ projects/illustrator-scripting/layer-manipulations.jsx
@@ -0,0 +1,50 @@
+// Removes "copy" fragments from layer names, skipping underscore-prefixed layers.
+// Run in Adobe Illustrator via File > Scripts > Other Script.
+
+(function () {
+    if (app.documents.length === 0) {
+        alert("Open a document before running this script.");
+        return;
+    }
+
+    processLayers(app.activeDocument.layers, removeCopyFromLayerName);
+
+    function processLayers(layers, callback) {
+        for (var i = layers.length - 1; i >= 0; i--) {
+            var layer = layers[i];
+
+            if (isSkippedLayer(layer)) {
+                continue;
+            }
+
+            processLayer(layer, callback);
+        }
+    }
+
+    function processLayer(layer, callback) {
+        var wasLocked = layer.locked;
+
+        try {
+            layer.locked = false;
+            callback(layer);
+
+            if (layer.layers.length > 0) {
+                processLayers(layer.layers, callback);
+            }
+        } finally {
+            layer.locked = wasLocked;
+        }
+    }
+
+    function isSkippedLayer(layer) {
+        return layer.name.charAt(0) === "_";
+    }
+
+    function removeCopyFromLayerName(layer) {
+        var copyIndex = layer.name.lastIndexOf("copy");
+
+        if (copyIndex >= 0) {
+            layer.name = layer.name.substring(0, copyIndex);
+        }
+    }
+}());
